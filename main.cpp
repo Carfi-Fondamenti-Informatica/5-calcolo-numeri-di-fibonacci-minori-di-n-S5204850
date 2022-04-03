d6f313375cc8c3503a9832af4605a2d7c77edd60
S5204850 <100359998+S5204850@users.noreply.github.com>
2022-04-03 18:12:12 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,14 @@ int main() {
     std::cin>>n;
 if(n>=2){
     int a=1; int b=1; int c; int d=3;
-    std::cout<<1<<std::endl;
-    std::cout<<1<<std::endl;
+    std::cout<<"1"<<std::endl;
+    std::cout<<"1"<<std::endl;
     while (n>=d) {
         c = a + b;
         std::cout << c << std::endl;
         b = a;
         a = c;
+        d=d+1;
 
     }
 
