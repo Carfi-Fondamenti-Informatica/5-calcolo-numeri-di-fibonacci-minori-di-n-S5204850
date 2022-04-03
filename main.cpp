c304638b1634e19ae2c0b441fa5e8aa8ec6f74c3
S5204850 <100359998+S5204850@users.noreply.github.com>
2022-04-03 17:56:28 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
-using namespace std;
 
 int main() {
-   return 0;
+    int n;
+    std::cin>>n;
+if(n>=2){
+    int a=1; int b=1; int c; int d=3;
+    std::cout<<"1"<<std::endl;
+    std::cout<<"1"<<std::endl;
+    while (n>=d) {
+        c = a + b;
+        std::cout << c << std::endl;
+        b = a;
+        a = c;
+        d = d + 1;
+    }
+
 }
+else
+    std::cout<<"errore"<<std::endl;
+
+    return 0;
+
+}
+
